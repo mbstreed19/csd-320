8c82a32921ec6342958e132a10e3336e66d24c29
Mikey Streed <mbstreed19@outlook.com>
2022-11-21 22:38:51 -0600

    started module 7 assignment

---
+++ module7/Password/Password.java
@@ -0,0 +1,47 @@
+/**
+ * Michael B. Streed Module 7 11-21-2022
+ *
+ * This program will take a string as input and then analyze it to see if it
+ * matches the following criteria must have at least 8 characters must have both
+ * letters and digits must contain at least one uppercase and at least one
+ * lowercase letter
+ *
+ */
+
+public class Password {
+    public static void main(String[] args) {
+
+        // Declare my method that will test the string
+        public static boolean checkString(final str) {
+            // these are variables that I will need
+            char ch;
+            boolean capitalFlag = false;
+            boolean lowerCaseFlag = false;
+            boolean numberFlag = false;
+            boolean stringLength = false;
+        // test length once. do not need to have this in loop
+            if (str.length() > 7) {
+                stringLength = true;
+            }
+            // a for loop to iterate over the string
+            for (int i = 0; i < str.length(); i++) {
+                ch = str.charAt(i);// this indexes the current character for evaluation.
+                // check for a digit
+                if (Character.isDigit(ch)) {
+                    numberFlag = true;
+                }
+                // check for an uppercase
+                else if (Character.isUpperCase(ch)) {
+                    capitalFlag = true;
+                    // check for lowercase
+                } else if (Character.isLowerCase(ch)) {
+                    lowerCaseFlag = true;
+                }
+                if (stringLength && capitalFlag && lowerCaseFlag && numberFlag){
+                    return true;
+                }
+            }
+            return false;
+        };
+    };
+}
