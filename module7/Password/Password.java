29292210f2ae8f9bb8f585503d202d4ead8a6058
Mikey Streed <mbstreed19@outlook.com>
2022-11-22 21:24:41 -0600

    I figured out my problem with my method.

--- module7/Password/Password.java
+++ module7/Password/Password.java
@@ -10,38 +10,40 @@
 
 public class Password {
     public static void main(String[] args) {
+        System.out.println(checkString("Butt4tuff"));
+    }
 
-        // Declare my method that will test the string
-        public static boolean checkString(final str) {
-            // these are variables that I will need
-            char ch;
-            boolean capitalFlag = false;
-            boolean lowerCaseFlag = false;
-            boolean numberFlag = false;
-            boolean stringLength = false;
+    // Declare my method that will test the string
+    public static boolean checkString(String str) {
+        // these are variables that I will need
+        char ch;
+        boolean capitalFlag = false;
+        boolean lowerCaseFlag = false;
+        boolean numberFlag = false;
+        boolean stringLength = false;
+
         // test length once. do not need to have this in loop
-            if (str.length() > 7) {
-                stringLength = true;
+        if (str.length() > 7) {
+            stringLength = true;
+        }
+        // a for loop to iterate over the string
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);// this indexes the current character for evaluation.
+            // check for a digit
+            if (Character.isDigit(ch)) {
+                numberFlag = true;
             }
-            // a for loop to iterate over the string
-            for (int i = 0; i < str.length(); i++) {
-                ch = str.charAt(i);// this indexes the current character for evaluation.
-                // check for a digit
-                if (Character.isDigit(ch)) {
-                    numberFlag = true;
-                }
-                // check for an uppercase
-                else if (Character.isUpperCase(ch)) {
-                    capitalFlag = true;
-                    // check for lowercase
-                } else if (Character.isLowerCase(ch)) {
-                    lowerCaseFlag = true;
-                }
-                if (stringLength && capitalFlag && lowerCaseFlag && numberFlag){
-                    return true;
-                }
+            // check for an uppercase
+            else if (Character.isUpperCase(ch)) {
+                capitalFlag = true;
+                // check for lowercase
+            } else if (Character.isLowerCase(ch)) {
+                lowerCaseFlag = true;
             }
-            return false;
-        };
-    };
+            if (stringLength && capitalFlag && lowerCaseFlag && numberFlag) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
