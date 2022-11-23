34ee8df585a94c48bb4109ff6beb6937e4c76ea6
Mikey Streed <mbstreed19@outlook.com>
2022-11-22 22:05:01 -0600

    completed mod 7 assignment

--- module7/Password/Password.java
+++ module7/Password/Password.java
@@ -1,3 +1,4 @@
+
 /**
  * Michael B. Streed Module 7 11-21-2022
  *
@@ -5,12 +6,22 @@
  * matches the following criteria must have at least 8 characters must have both
  * letters and digits must contain at least one uppercase and at least one
  * lowercase letter
- *
- */
+ * */
+import java.util.Scanner;// gotta bring in the scanner if we want input
 
 public class Password {
     public static void main(String[] args) {
-        System.out.println(checkString("Butt4tuff"));
+        Scanner userInput = new Scanner(System.in);// inits the scanner to take input
+        System.out.println("Enter String for analysis for use as Password");// input prompt
+        String userString = userInput.nextLine();// stores input in variable
+        boolean password = (checkString(userString));// assigns value from checkString method to var password
+        if (password) {
+            System.out.println("This password is acceptable.");
+        } else {
+            System.out.println(
+                    "This password is not acceptable. It needs at least one number, lowercase letter, uppercase letter, and needs to be at least 8 digits long");
+        }
+
     }
 
     // Declare my method that will test the string
@@ -40,10 +51,13 @@ public class Password {
             } else if (Character.isLowerCase(ch)) {
                 lowerCaseFlag = true;
             }
+            // checks if all conditions are met, if so returns a true value.
             if (stringLength && capitalFlag && lowerCaseFlag && numberFlag) {
                 return true;
             }
         }
+        // if any one of the conditions are not met then the method returns a false
+        // value
         return false;
     }
 }
