351cc367faba74c61fbd31e63b5edaf74042a0d0
Mikey Streed <mbstreed19@outlook.com>
2022-11-28 21:54:22 -0600

    started module 9 assignment

---
+++ module9/Arrays/Array.java
@@ -0,0 +1,17 @@
+// Michael B. Streed
+// Module 9 assignment
+// 11-28-2000
+/**
+ * The goal of this assignment is to be able to show my understanding of how
+ * arrays work by writing a program that reads and fills an array with twenty
+ * integers, and then using the array elements calculate and display the
+ * following: the highest value entered, the lowest value entered, the average
+ * of the numbers entered, the sum of the numbers entered, and the information
+ * must be pretty and easy to understand.
+ *
+ */
+public class Array {
+    public static void main(String[] args) {
+
+    }
+}
